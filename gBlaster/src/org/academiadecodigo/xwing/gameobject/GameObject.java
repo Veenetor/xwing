41038524bcf679110115b7d88f2dd15b7b17287d
Andre Moura <apccfm@gmail.com>
2020-02-12 09:35:11 +0000

    Game Object defined

--- gBlaster/src/org/academiadecodigo/xwing/gameobject/GameObject.java
+++ gBlaster/src/org/academiadecodigo/xwing/gameobject/GameObject.java
@@ -1,4 +1,49 @@
 package org.academiadecodigo.xwing.gameobject;
 
+import org.academiadecodigo.xwing.grid.Grid;
+import org.academiadecodigo.xwing.grid.GridColor;
+import org.academiadecodigo.xwing.grid.GridPosition;
+import org.academiadecodigo.xwing.simplegfx.SimpleGfxGrid;
+
 public abstract class GameObject {
+
+
+    private GridPosition pos;
+    private SimpleGfxGrid gfxPos;
+    private int speed;
+    private ObjectType type;
+    private boolean destroyed;
+
+    // CONSTRUCTOR
+    public GameObject (GridPosition pos, ObjectType type) {
+        this.pos = pos;
+        this.type = type;
+
+        pos.setColor(type.getColor());
+
+    }
+
+
+
+    // METHODS
+
+    /*
+
+    Fuck me all (collision) detection!!
+
+
+    */
+
+
+    public void move () {
+
+    }
+
+
+    /*
+
+    Override to String();
+    
+    */
+
 }
